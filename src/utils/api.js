f616b192d56f4f8b8cd6c9002bf39c277cb1ec2b
asaadmansour <as89aadmansour@gmail.com>
2026-04-17 21:31:08 +0200

    added refresh access endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -17,4 +17,35 @@ api.interceptors.request.use(
   }
 );
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; 
+
+      try {
+        const res = await axios.post(
+          `${import.meta.env.VITE_BASE_BACKEND_URL}/auth/token/refresh/`,
+          {},
+          { withCredentials: true }
+        );
+
+        const newAccessToken = res.data.access;
+        localStorage.setItem('access_token', newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem('access_token');
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
